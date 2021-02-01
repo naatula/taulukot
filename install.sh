028f2fdc3d4cd576f85198df3130269d7823ed9a
Simo Naatula <git@naatu.la>
2021-02-01 02:00:13 +0200

    Use Downloads folder

--- install.sh
+++ install.sh
@@ -10,11 +10,11 @@ resource(){
 if [ ! -f ~/.maol/web/index.html ]; then
   brew --version >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   type unsquashfs >/dev/null 2>&1 || brew install squashfs
-  mkdir -p ~/.maol
   echo "Ladataan asennuspakettia..."
-  curl http://static.abitti.fi/etcher-usb/koe-etcher.zip -C - --progress-bar --output ~/.maol/koe-etcher.zip
+  curl http://static.abitti.fi/etcher-usb/koe-etcher.zip -C - --progress-bar --output ~/Downloads/koe-etcher.zip
   echo "Asennetaan..."
-  unzip -q ~/.maol/koe-etcher.zip ytl/koe.img -d ~/.maol/
+  mkdir -p ~/.maol
+  unzip -q ~/Downloads/koe-etcher.zip ytl/koe.img -d ~/.maol/
   VOLNAME=`hdiutil attach ~/.maol/ytl/koe.img | grep -o '/Volumes/ABITTI.*'`
   unsquashfs -q -d ~/.maol/squashfs $VOLNAME/live/filesystem.squashfs /usr/local/share/maol-digi
   mv ~/.maol/squashfs/usr/local/share/maol-digi/content ~/.maol/web
@@ -23,7 +23,7 @@ if [ ! -f ~/.maol/web/index.html ]; then
   resource com.naatula.maol.plist ~/Library/LaunchAgents/com.naatula.maol.plist
   launchctl load ~/Library/LaunchAgents/com.naatula.maol.plist
   launchctl start com.naatula.maol
-  rm -r ~/.maol/squashfs ~/.maol/ytl ~/.maol/koe-etcher.zip
+  rm -r ~/.maol/squashfs ~/.maol/ytl
   echo "\nMAOL on asennettu osoitteeseen http://localhost:3401"
   open 'http://localhost:3401/'
 else
