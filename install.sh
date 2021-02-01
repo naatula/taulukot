6fd64dbc9784b367dbf35c14f971446f20a175b9
Simo Naatula <git@naatu.la>
2021-02-01 16:49:17 +0200

    Improve uninstaller

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ if [[ "$(uname)" != "Darwin" ]]; then
   echo "Only supported on macOS"
   exit 1
 fi
+
 resource(){
   if [ -f "./$1" ]; then
     cp "./$1" "$2"
@@ -11,45 +12,54 @@ resource(){
   fi
   return 0
 }
-if [ ! -f ~/.maol/web/index.html ]; then
-  brew --version >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  type unsquashfs >/dev/null 2>&1 || brew install squashfs
-  mkdir -p ~/.maol
-  if [ -f ~/Downloads/koe-etcher.zip ]; then
-    echo "Käytetään asennuspakettia ~/Downloads/koe-etcher.zip"
-    DL="~/Downloads/koe-etcher.zip"
-  else
-    echo "Ladataan asennuspakettia..."
-    curl http://static.abitti.fi/etcher-usb/koe-etcher.zip -C - --progress-bar --output ~/.maol/koe-etcher.zip
-    DL="~/.maol/koe-etcher.zip"
-  fi
-  echo "Asennetaan..."
-  unzip -qo $DL ytl/koe.img -d ~/.maol/ || { echo "Viallinen asennuspaketti" ; exit 1 }
-  VOLNAME=`hdiutil attach ~/.maol/ytl/koe.img | grep -o '/Volumes/ABITTI.*'`
-  unsquashfs -q -d ~/.maol/squashfs $VOLNAME/live/filesystem.squashfs /usr/local/share/maol-digi
-  mv ~/.maol/squashfs/usr/local/share/maol-digi/content ~/.maol/web
-  diskutil quiet unmount $VOLNAME
-  mkdir -p ~/Library/LaunchAgents/
-  resource com.naatula.maol.plist ~/Library/LaunchAgents/com.naatula.maol.plist
-  launchctl load ~/Library/LaunchAgents/com.naatula.maol.plist
-  launchctl start com.naatula.maol
-  rm -rf ~/.maol/squashfs ~/.maol/ytl ~/.maol/koe-etcher.zip
-  echo "\nMAOL on asennettu osoitteeseen http://localhost:3401"
-  open 'http://localhost:3401/'
-else
-  echo '\nMAOL on jo asennettu'
-  echo "Poistetaanko asennus? [k/e]"
+
+if [ -f ~/.maol/web/index.html ]; then
+  echo '\nMAOL on jo asennettu\n[P] Poista asennus\n[U] Poista ja asenna uudelleen'
   read -k A
   echo
-  if [ "$A" = "k" ]; then
+  if [[ "$A" =~ [pPuU] ]]; then
     echo "Poistetaan palvelu..."
     launchctl stop com.naatula.maol
     launchctl unload ~/Library/LaunchAgents/com.naatula.maol.plist
     rm -f ~/Library/LaunchAgents/com.naatula.maol.plist
     echo "Poistetaan tiedostot..."
-    rm -r ~/.maol
+    rm -rf ~/.maol
     rm -f ~/Desktop/MAOL.webloc 
-    echo "Asennus poistettu\nSuorita sama komento uudelleen uudelleenasentaaksesi"
+    echo "Asennus poistettu"
   fi
+  if [[ ! "$A" =~ [uU] ]]; then exit 0; fi
 fi
-echo '\nTämän ikkunan voi nyt sulkea'
+
+brew --version >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+type unsquashfs >/dev/null 2>&1 || brew install squashfs
+mkdir -p ~/.maol
+if [ -f ~/Downloads/koe-etcher.zip ]; then
+  echo "Käytetään asennuspakettia ~/Downloads/koe-etcher.zip"
+  DL=~/Downloads/koe-etcher.zip
+else
+  echo "Ladataan asennuspakettia..."
+  curl http://static.abitti.fi/etcher-usb/koe-etcher.zip -C - --progress-bar --output ~/.maol/koe-etcher.zip
+  DL=~/.maol/koe-etcher.zip
+fi
+echo "Asennetaan..."
+unzip -qo $DL ytl/koe.img -d ~/.maol/ || {
+  echo "Viallinen asennuspaketti"
+  if [[ $DL == ~/Downloads/koe-etcher.zip ]]; then
+    echo "Poista Lataukset-kansiosta koe-etcher.zip ja yritä uudelleen"
+  else
+    rm -rf ~/.maol/koe-etcher.zip
+  fi
+  exit 1
+}
+VOLNAME=`hdiutil attach ~/.maol/ytl/koe.img | grep -o '/Volumes/ABITTI.*'`
+unsquashfs -q -d ~/.maol/squashfs $VOLNAME/live/filesystem.squashfs /usr/local/share/maol-digi
+mv ~/.maol/squashfs/usr/local/share/maol-digi/content ~/.maol/web
+diskutil quiet unmount $VOLNAME
+mkdir -p ~/Library/LaunchAgents/
+resource com.naatula.maol.plist ~/Library/LaunchAgents/com.naatula.maol.plist
+launchctl load ~/Library/LaunchAgents/com.naatula.maol.plist
+launchctl start com.naatula.maol
+rm -rf ~/.maol/squashfs ~/.maol/ytl ~/.maol/koe-etcher.zip
+echo "\nMAOL on asennettu osoitteeseen http://localhost:3401"
+open 'http://localhost:3401/'
+exit 0
